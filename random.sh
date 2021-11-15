a52b659e5c514f046d45c0df1b8ffcd47a56314d
jlchen5 <61578993+jlchen5@users.noreply.github.com>
2021-11-15 12:27:50 +0800

    Update random.sh

--- random.sh
+++ random.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+work_dir=`pwd`
+
+if [! -n "genes.txt"]
+   then touch genes.txt
+   else echo "The gene.txt does not exist"
+fi
 
 for i in {1..100};do
 
-	shuf -n1096 genes.txt > rm_gene_$i.txt
+	shuf -n1096 $work_dir/genes.txt > $work_dir/rm_gene_$i.txt
 	
-	cat rm_gene_$i.txt |while read line ;do grep -x $line /home/project/genes.txt;done |sort -u |wc > rmgenes_$i.txt
+	cat $work_dir/rm_gene_$i.txt |while read line ;do grep -x $line $work_dir/genes.txt;done |sort -u |wc > $i.txt
 
 
 done
